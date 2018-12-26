9769b3515e505f97781cb8887ac114a4f444431d
Barnabas Kim <barnabaskim95@gmail.com>
2018-12-25 18:04:16 -0600

    added data persistence for voting

--- src/components/Thumbs.js
+++ src/components/Thumbs.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {FaThumbsUp } from 'react-icons/fa';
+import ls from 'local-storage'
 
 class Thumbs extends React.Component {
-  state = { count: 0 }
+  state = { 
+    count: ls.get(this.props.name) || 0
+  }
 
   increment = () => {
     this.setState({
       count: this.state.count + 1
     });
+    ls.set(this.props.name,this.state.count)
   }
-
   render() {
     return (
       <div>
@@ -18,7 +21,7 @@ class Thumbs extends React.Component {
           <button class='button' onClick={this.increment}><div class='row'><div><FaThumbsUp size={5}/></div><div><p class='bio-paragraph'>Yes!</p></div></div></button>
         </div>
         <div>
-          <p class='bio-paragraph'>{this.state.count} people have said Yes!</p>
+          <p class='bio-paragraph'> {ls.get(this.props.name) || 0} people have said Yes!</p>
         </div>
       </div>
     )
